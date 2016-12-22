7a9a8806ba190e762b87834c71da428dcf3765d9
zhangwei <zwzw.love@163.com>
2016-12-22 23:09:11 +0800

    删除无用类文件
    Signed-off-by: zhangwei <zwzw.love@163.com>

---
+++ kq-parent/kq-war/src/test/java/com/dc/kq/pinche/TestDemo.java
@@ -0,0 +1,20 @@
+package com.dc.kq.pinche;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+/**
+ * 测试类demo
+ * @author david
+ *
+ */
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class TestDemo {
+
+	// @Autowired
+	
+	// @Test
+	
+
+}
